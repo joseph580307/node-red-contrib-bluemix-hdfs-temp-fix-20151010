0f4cda7e136ca7334ff986055c8d6cbecbf43239
Joseph Chang <changhs@tw.ibm.com>
2015-10-10 08:58:57 +0000

    back to 0

--- application/47-ibmhdfs-cf.js
+++ application/47-ibmhdfs-cf.js
@@ -14,48 +14,49 @@
  * limitations under the License.
  **/
 
-/*globals putPostHeaders:true */
 var RED = require(process.env.NODE_RED_HOME+"/red/red");
 //var util = require("util");
-var httpForRead = require("follow-redirects").http;
-var httpsForRead = require("follow-redirects").https;
-
-var http = require("http");
-var https = require("https");
-
-var urllib = node.log("host = " + option1.host);
+var http = require("follow-redirects").http;
+var https = require("follow-redirects").https;
+var urllib = require("url");
 var getBody = require('raw-body');
 //var mustache = require("mustache");
-var fs = require("fs");
+//var fs = require("fs");
+
+//var cors = require('cors');
+
+//var cfEnv = require("cf-env");
+// Load the services VCAP from the CloudFoundry environment
+//var services = cfEnv.getCore().services || {};
 
 var cfenv = require("cfenv");
 // Load the services VCAP from the CloudFoundry environment
 var appenv = cfenv.getAppEnv();
 var services = appenv.services || {};
+var userServices = services['IBM Analytics for Hadoop'];
 
-var userServices = services['Analytics for Apache Hadoop'];
 var bigcredentials = false;
 
 if (userServices) {
 	for(var i = 0, l = userServices.length; i < l; i++){
 		var service = userServices[i];
 		if(service.credentials){
-			if(service.credentials.WebhdfsUrl){
+			if(service.credentials.HttpfsUrl){
 				bigcredentials = service.credentials;
 				console.log("BIG CREDENTIALS FOUND.....");
 				break;
 			}
 		}
 	}
-} 
+}
 /*
 else {
-	var vcapData = fs.readFileSync("credentials.cfg"), fileContents;
+	var data = fs.readFileSync("credentials.cfg"), fileContents;
 	try {
-		fileContents = JSON.parse(vcapData);
+		fileContents = JSON.parse(data);
 
 		bigcredentials = {};
-		bigcredentials.WebhdfsUrl = fileContents.WebhdfsUrl;
+		bigcredentials.HttpfsUrl = fileContents.HttpfsUrl;
 		bigcredentials.userid = fileContents.userid;
 		bigcredentials.password = fileContents.password;
 
@@ -74,8 +75,6 @@ RED.httpAdmin.get('/iotFoundation/bigdata', function(req,res) {
 
 
 function HDFSRequestInNode(n) {
-//	var http = require("follow-redirects").http;
-//	var https = require("follow-redirects").https;
 	RED.nodes.createNode(this,n);
 
 	this.filename = n.filename;
@@ -86,40 +85,37 @@ function HDFSRequestInNode(n) {
 	if (this.format) {
 		options['encoding'] = this.format;
 	}
-	node.status({fill:"grey",shape:"dot",text:"done"});
 	this.on("input",function(msg) {
-		node.status({fill:"blue",shape:"ring",text:"requesting"});
+		node.status({fill:"blue",shape:"dot",text:"requesting"});
 		var filename = msg.filename || this.filename;
 		var homeDirectory = "user/" + bigcredentials.userid;
 
-		var url = bigcredentials.WebhdfsUrl;
+		var url = bigcredentials.HttpfsUrl;
 		url = url + homeDirectory + filename;
-		
-		node.log("filename777 = " + filename);
+
+		node.log("filename = " + filename);
 		if (filename == "") {
 			node.warn('No filename specified');
-			node.status({fill:"grey",shape:"dot",text:""});
 		} else {
 			var opts = urllib.parse(url + "?op=OPEN");
 			opts.method = "GET";
 			opts.headers = {};
 			var payload = null;
 			opts.auth = bigcredentials.userid+":"+(bigcredentials.password||"");
-			var req = ((/^https/.test(url))?httpsForRead:httpForRead).request(opts,function(res) {
-				
-				node.log("req = " + req);
+			var req = ((/^https/.test(url))?https:http).request(opts,function(res) {
 				if(options['encoding'] == 'utf8') {
 					res.setEncoding('utf8');
 				} else {
 					res.setEncoding('binary');
 				}
 				msg.statusCode = res.statusCode;
+				node.log("Status = " + msg.statusCode);
 				msg.headers = res.headers;
+				if(msg.payload !== null) {
 
-				if(msg.payload === null) {
+				} else {
 					msg.payload = "";
-				} 
-				
+				}
 				var begun = false;
 				res.on('data',function(chunk) {
 					node.status({fill:"green",shape:"dot",text:"connected"});
@@ -129,15 +125,20 @@ function HDFSRequestInNode(n) {
 						msg.payload = chunk;
 						begun = true;
 					}
+
 					payload = msg.payload;
+
 					node.log("Payload in DATA = " + msg.payload);
 				});
 				res.on('end',function() {
 					node.send(msg);
-					node.status({fill:"grey",shape:"dot",text:"read"});
+					node.log("Payload in END = " + msg.payload);
+					if(msg.payload.boolean == false) {
+						node.error("Unable to delete the file");
+					}
+					node.status({fill:"grey",shape:"dot",text:"done"});
 				});
 			});
-			node.log("req = " + req);
 			req.on('error',function(err) {
 				msg.payload = err.toString();
 				msg.statusCode = err.code;
@@ -166,17 +167,14 @@ function HDFSRequest(n) {
 	if (this.format) {
 		options['encoding'] = this.format;
 	}
-	node.status({fill:"grey",shape:"dot",text:"done"});
 	this.on("input",function(msg) {
 		var that = this;
-		node.status({fill:"blue",shape:"ring",text:"requesting"});
+		node.status({fill:"blue",shape:"dot",text:"requesting"});
 		var filename = msg.filename || this.filename;
 		var homeDirectory = "user/" + bigcredentials.userid;
 
-		var url = bigcredentials.WebhdfsUrl;
+		var url = bigcredentials.HttpfsUrl;
 		url = url + homeDirectory + filename;
-		node.log("url=" + url)
-		
 
 		var fileChanged = false;
 		node.log("filename = " + filename);
@@ -201,12 +199,10 @@ function HDFSRequest(n) {
 				data += "\n";
 			}
 			if (msg.hasOwnProperty('delete')) {
-				node.warn(filename + " deleted from HDFS");
 				opts = urllib.parse(url + "?op=DELETE");
 				opts.method = "DELETE";
 				opts.headers = {};
 				opts.auth = bigcredentials.userid+":"+(bigcredentials.password||"");
-				node.status({fill:"grey",shape:"dot",text:"deleted"});
 			}
 			else {
 				if(n.overwriteFile)  {
@@ -216,7 +212,6 @@ function HDFSRequest(n) {
 					opts.auth = bigcredentials.userid+":"+(bigcredentials.password||"");
 					opts.headers['transfer-encoding'] = 'chunked';
 					opts.headers['content-type'] = 'application/octet-stream';
-					opts.encoding = options.encoding;
 				} else {
 					opts = urllib.parse(url + "?op=APPEND&data=true");
 					opts.method = "POST";
@@ -224,65 +219,16 @@ function HDFSRequest(n) {
 					opts.auth = bigcredentials.userid+":"+(bigcredentials.password||"");
 					opts.headers['transfer-encoding'] = 'chunked';
 					opts.headers['content-type'] = 'application/octet-stream';				
-					opts.encoding = options.encoding;
+
 				}
 				if (opts.headers['content-length'] == null) {
 					opts.headers['content-length'] = Buffer.byteLength(data);
 				}
 			}
-			
-
 			var req = ((/^https/.test(url))?https:http).request(opts,function(res) {
-				if(opts['encoding'] == 'utf8') {
-					res.setEncoding('utf8');
-				} else {
-					res.setEncoding('binary');
-				}
-				
+				res.setEncoding('utf8');
 				msg.statusCode = res.statusCode;
 				msg.headers = res.headers;
-				node.log("statusCode = " + msg.statusCode + " " + "statusheaders = " + msg.statusheaders);
-
-				if(msg.statusCode == 307) {
-					node.status({fill:"green",shape:"dot",text:"connected"});
-					var newLocation = res.headers.location;
-					putPostHeaders = {
-						'Content-Type' : 'application/octet-stream',
-						'Content-Length' : Buffer.byteLength(data)
-					};
-					
-					var url_parts = urllib.parse(newLocation, true);
-
-					var option = {
-						path : url_parts.path,
-						host : url_parts.hostname,
-						port : url_parts.port,
-						method : opts.method,
-						headers : putPostHeaders,
-						auth : bigcredentials.userid+":"+(bigcredentials.password||""),
-						encoding : opts.encoding,
-					};
-
-//					req.write(data);
-					req.end();
-
-					var reqPut = ((/^https/.test(url))?https:http).request(option, function(res) {
-						console.log("Status code", res.statusCode );
-						node.log("Status code", res.statusCode);
-
-						res.on('data', function(d) {
-							console.info('PUT result:\n');
-							process.stdout.write(d);
-							console.info('\n\nPUT completed');
-						});
-
-					});
-					node.log("Content = " + data + " ends");
-					reqPut.write(data);
-					reqPut.end();
-					node.status({fill:"grey",shape:"dot",text:"inserted / updated"});
-				}
-
 
 				res.on('data',function(chunk) {
 					node.status({fill:"green",shape:"dot",text:"connected"});
@@ -290,7 +236,7 @@ function HDFSRequest(n) {
 					if(msg.statusCode == 404 ) {
 						node.error("File doesnt exist, so creating one");	
 
-											
+						
 						opts = null;				
 						opts = urllib.parse(url + "?op=CREATE&data=true");
 						opts.method = "PUT";
@@ -301,79 +247,24 @@ function HDFSRequest(n) {
 						if (opts.headers['content-length'] == null) {
 							opts.headers['content-length'] = Buffer.byteLength(data);
 						}
-					 
 						var req1 = ((/^https/.test(url))?https:http).request(opts,function(res1) {
 							res1.setEncoding('utf8');
 							msg.statusCode = res1.statusCode;
 							msg.headers = res1.headers;
-							node.log("Status code 1 = " + msg.statusCode);
-							node.error("Receive Status Code 1: "+ msg.statusCode);
-							
-
-						//	Reply to the CREATE OP response 
-						if(msg.statusCode == 307) {
-								node.log("Get status Code 307");
-								var newLocation1 = res1.headers.location;
-								node.log("New location: " + newLocation1);
-								putPostHeaders = {
-									'Content-Type' : 'application/octet-stream',
-									'Content-Length' : Buffer.byteLength(data)
-							   	};
-   
-   								var url_parts1 = urllib.parse(newLocation1, true);
-
-								var option1 = {
-									path : url_parts1.path,
-									host : url_parts1.hostname,
-									port : url_parts1.port,
-									method : "PUT",
-									headers : putPostHeaders,
-									auth : bigcredentials.userid+":"+(bigcredentials.password||""),
-									encoding : opts.encoding,
-									};
-									node.log("option1 hostname = " + option1.hostname);
-									node.log("option1 path = " + option1.path);
-									node.log("option1 port= " + option1.port);
-
-                                    req1.end();
-
-									var reqPut1 = ((/^https/.test(url))?https:http).request(option1, function(res1) {
-										console.log("Status code 2", res1.statusCode );
-										node.log("Status code 2", res1.statusCode);
-
-										res1.on('data', function(d) {
-											console.info('PUT result:\n');
-											process.stdout.write(d);
-											console.info('\n\nPUT completed');
-										});
-
-									});
-								node.log("Content = " + data + " ends");
-								reqPut1.write(data);
-								reqPut1.end();
-								node.status({fill:"grey",shape:"dot",text:"inserted / updated"});
-							}
-						
-						
 
 							res1.on('data',function(chunk) {
 								node.status({fill:"green",shape:"dot",text:"connected"});
-								node.log("On Data ");
-								
-								
+								node.log("Status code = " + msg.statusCode);
 								if(msg.statusCode == 404 ) {
 									node.error("Unable to create the file......");	
 									fileChanged = false;
 								} else {
-									
 									data += chunk;
 									fileChanged = true
 								}
 								node.log("Payload in DATA AGAIN= " + data);
-								node.log("Exiting....");
 							});
 							res1.on('end',function() {
-								node.log("Before sending...");
 								node.send(msg);
 								node.log("Payload in END AGAIN= " + data);
 								node.status({fill:"grey",shape:"dot",text:"done"});
@@ -397,11 +288,9 @@ function HDFSRequest(n) {
 					}
 					node.log("Payload in DATA = " + data);
 				});
-
-
 				res.on('end',function() {
 					node.send(msg);
-					node.log("Payload in END = " + data);
+//					node.log("Payload in END = " + data);
 					node.status({fill:"grey",shape:"dot",text:"done"});
 				});
 			});
@@ -417,7 +306,6 @@ function HDFSRequest(n) {
 			}
 			req.end();
 		}
-
 	});
 }
 
